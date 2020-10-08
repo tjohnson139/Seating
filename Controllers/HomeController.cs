d05cb592497f878e5f4de4939d22596cc7ce7689
tjohnson139 <tylerjohnson139@gmail.com>
2020-10-08 16:48:22 -0700

    Add the ability to take someone back off the floor once accidentally added.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -626,6 +626,319 @@ namespace Seating.Controllers
         }
 
         [HttpPost]
+        public ActionResult empSentCxl(int breakID)
+        {
+            try
+            {
+                var oldBreakModel = db.Breaks.Where(x => x.BreakID == breakID).FirstOrDefault();
+                oldBreakModel.EmpSent = false;
+                db.SaveChanges();
+
+                var breaksList = db.Breaks.ToList();
+                foreach (var item in breaksList)
+                {
+                    Break mdl = new Break();
+                    mdl.BreakID = item.BreakID;
+                    mdl.Employee = item.Employee;
+                    mdl.TimeEntered = item.TimeEntered;
+                    mdl.TimeCleared = item.TimeCleared;
+                    mdl.PositionID = item.PositionID;
+                    mdl.EmpSent = item.EmpSent;
+                    if (db.Breaks != null)
+                    {
+                        db.Breaks.Add(mdl);
+                    }
+                    db.Breaks.Add(mdl);
+                }
+                var employee = db.Employees.ToList();
+                foreach (var item in employee)
+                {
+                    Employee mdl = new Employee();
+                    mdl.EmployeeID = item.EmployeeID;
+                    mdl.FirstName = item.FirstName;
+                    mdl.LastName = item.LastName;
+                    mdl.NotActive = item.NotActive;
+                    mdl.Force = item.Force;
+                    mdl.DisplayName = item.DisplayName;
+                    if (db.Employees != null)
+                    {
+                        db.Employees.Add(mdl);
+                    }
+                }
+                var dthmodel = db.Dths.ToList();
+                foreach (var item in dthmodel)
+                {
+                    Dth mdl = new Dth();
+                    mdl.DthID = item.DthID;
+                    mdl.Employee = item.Employee;
+                    mdl.TimeCleared = item.TimeCleared;
+                    mdl.TimeEntered = item.TimeEntered;
+                    mdl.PositionID = item.PositionID;
+                    mdl.EmpSent = item.EmpSent;
+                    if (db.Dths != null)
+                    {
+                        db.Dths.Add(mdl);
+                    }
+                }
+
+                var lunchModel = db.Lunches.OrderBy(x => x.LunchTime);
+                foreach (var item in lunchModel)
+                {
+                    Lunch mdl = new Lunch();
+                    mdl.Employee = item.Employee;
+                    mdl.LunchID = item.LunchID;
+                    mdl.PositionID = item.PositionID;
+                    mdl.LunchTime = item.LunchTime;
+                    mdl.LongerLunch = item.LongerLunch;
+                    mdl.Double = item.Double;
+                    mdl.TimeEntered = item.TimeEntered;
+                    mdl.EmpSent = item.EmpSent;
+                    mdl.TimeCleared = item.TimeCleared;
+                    if (db.Lunches != null)
+                    {
+                        db.Lunches.Add(mdl);
+                    }
+                }
+                var positionModel = db.Positions.ToList();
+                foreach (var item in positionModel)
+                {
+                    Position mdl = new Position();
+                    mdl.PositionID = item.PositionID;
+                    mdl.PositionName = item.PositionName;
+                    if (db.Positions != null)
+                    {
+                        db.Positions.Add(mdl);
+                    }
+                }
+
+                ViewBag.EmployeesNames = db.Employees.Where(x => x.NotActive == false).OrderBy(x => x.DisplayName).ToList();
+                ViewBag.PositionNames = db.Positions.OrderBy(x => x.PositionName).ToList();
+                ViewBag.LunchTimes = db.Lunches.Where(x => x.TimeCleared == null).OrderBy(x => x.LunchTime).ToList();
+                ViewBag.Dths = db.Dths.Where(x => x.TimeCleared == null).OrderBy(x => x.TimeEntered).ToList();
+                ViewBag.Breaks = db.Breaks.Where(x => x.TimeCleared == null).OrderBy(x => x.TimeEntered).ToList();
+                ViewBag.Lunches = db.Lunches.Where(x => x.TimeCleared == null).OrderBy(x => x.LunchTime).ToList();
+                ViewBag.breakOffFloor = db.Breaks.Where(x => x.EmpSent == true && x.TimeCleared == null).ToList();
+                ViewBag.dthOffFloor = db.Dths.Where(x => x.EmpSent == true && x.TimeCleared == null).ToList();
+                ViewBag.lunchOffFloor = db.Lunches.Where(x => x.EmpSent == true && x.TimeCleared == null).ToList();
+
+                string partialHtml = Common.RenderRazorViewToString(ControllerContext, "~/Views/Home/_DropDowns.cshtml");
+                return Json(new { success = true, html = partialHtml }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false });
+            }
+        }
+
+        public ActionResult dthSentCxl(int dthID)
+        {
+            try
+            {
+                var oldDthModel = db.Dths.Where(x => x.DthID == dthID).FirstOrDefault();
+                oldDthModel.EmpSent = false;
+                db.SaveChanges();
+
+                var breaksList = db.Breaks.ToList();
+                foreach (var item in breaksList)
+                {
+                    Break mdl = new Break();
+                    mdl.BreakID = item.BreakID;
+                    mdl.Employee = item.Employee;
+                    mdl.TimeEntered = item.TimeEntered;
+                    mdl.TimeCleared = item.TimeCleared;
+                    mdl.PositionID = item.PositionID;
+                    mdl.EmpSent = item.EmpSent;
+                    if (db.Breaks != null)
+                    {
+                        db.Breaks.Add(mdl);
+                    }
+                    db.Breaks.Add(mdl);
+                }
+                var employee = db.Employees.ToList();
+                foreach (var item in employee)
+                {
+                    Employee mdl = new Employee();
+                    mdl.EmployeeID = item.EmployeeID;
+                    mdl.FirstName = item.FirstName;
+                    mdl.LastName = item.LastName;
+                    mdl.NotActive = item.NotActive;
+                    mdl.Force = item.Force;
+                    mdl.DisplayName = item.DisplayName;
+                    if (db.Employees != null)
+                    {
+                        db.Employees.Add(mdl);
+                    }
+                }
+                var dthmodel = db.Dths.ToList();
+                foreach (var item in dthmodel)
+                {
+                    Dth mdl = new Dth();
+                    mdl.DthID = item.DthID;
+                    mdl.Employee = item.Employee;
+                    mdl.TimeCleared = item.TimeCleared;
+                    mdl.TimeEntered = item.TimeEntered;
+                    mdl.PositionID = item.PositionID;
+                    mdl.EmpSent = item.EmpSent;
+                    if (db.Dths != null)
+                    {
+                        db.Dths.Add(mdl);
+                    }
+                }
+
+                var lunchModel = db.Lunches.OrderBy(x => x.LunchTime);
+                foreach (var item in lunchModel)
+                {
+                    Lunch mdl = new Lunch();
+                    mdl.Employee = item.Employee;
+                    mdl.LunchID = item.LunchID;
+                    mdl.PositionID = item.PositionID;
+                    mdl.LunchTime = item.LunchTime;
+                    mdl.LongerLunch = item.LongerLunch;
+                    mdl.Double = item.Double;
+                    mdl.TimeEntered = item.TimeEntered;
+                    mdl.EmpSent = item.EmpSent;
+                    mdl.TimeCleared = item.TimeCleared;
+                    if (db.Lunches != null)
+                    {
+                        db.Lunches.Add(mdl);
+                    }
+                }
+                var positionModel = db.Positions.ToList();
+                foreach (var item in positionModel)
+                {
+                    Position mdl = new Position();
+                    mdl.PositionID = item.PositionID;
+                    mdl.PositionName = item.PositionName;
+                    if (db.Positions != null)
+                    {
+                        db.Positions.Add(mdl);
+                    }
+                }
+
+                ViewBag.EmployeesNames = db.Employees.Where(x => x.NotActive == false).OrderBy(x => x.DisplayName).ToList();
+                ViewBag.PositionNames = db.Positions.OrderBy(x => x.PositionName).ToList();
+                ViewBag.LunchTimes = db.Lunches.Where(x => x.TimeCleared == null).OrderBy(x => x.LunchTime).ToList();
+                ViewBag.Dths = db.Dths.Where(x => x.TimeCleared == null).OrderBy(x => x.TimeEntered).ToList();
+                ViewBag.Breaks = db.Breaks.Where(x => x.TimeCleared == null).OrderBy(x => x.TimeEntered).ToList();
+                ViewBag.Lunches = db.Lunches.Where(x => x.TimeCleared == null).OrderBy(x => x.LunchTime).ToList();
+                ViewBag.breakOffFloor = db.Breaks.Where(x => x.EmpSent == true && x.TimeCleared == null).ToList();
+                ViewBag.dthOffFloor = db.Dths.Where(x => x.EmpSent == true && x.TimeCleared == null).ToList();
+                ViewBag.lunchOffFloor = db.Lunches.Where(x => x.EmpSent == true && x.TimeCleared == null).ToList();
+
+                string partialHtml = Common.RenderRazorViewToString(ControllerContext, "~/Views/Home/_DropDowns.cshtml");
+                return Json(new { success = true, html = partialHtml }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false });
+            }
+        }
+
+        public ActionResult lunchSentCxl(int lunchID)
+        {
+            try
+            {
+                var oldlunchModel = db.Lunches.Where(x => x.LunchID == lunchID).FirstOrDefault();
+                oldlunchModel.EmpSent = false;
+                db.SaveChanges();
+
+                var breaksList = db.Breaks.ToList();
+                foreach (var item in breaksList)
+                {
+                    Break mdl = new Break();
+                    mdl.BreakID = item.BreakID;
+                    mdl.Employee = item.Employee;
+                    mdl.TimeEntered = item.TimeEntered;
+                    mdl.TimeCleared = item.TimeCleared;
+                    mdl.PositionID = item.PositionID;
+                    mdl.EmpSent = item.EmpSent;
+                    if (db.Breaks != null)
+                    {
+                        db.Breaks.Add(mdl);
+                    }
+                    db.Breaks.Add(mdl);
+                }
+                var employee = db.Employees.ToList();
+                foreach (var item in employee)
+                {
+                    Employee mdl = new Employee();
+                    mdl.EmployeeID = item.EmployeeID;
+                    mdl.FirstName = item.FirstName;
+                    mdl.LastName = item.LastName;
+                    mdl.NotActive = item.NotActive;
+                    mdl.Force = item.Force;
+                    mdl.DisplayName = item.DisplayName;
+                    if (db.Employees != null)
+                    {
+                        db.Employees.Add(mdl);
+                    }
+                }
+                var dthmodel = db.Dths.ToList();
+                foreach (var item in dthmodel)
+                {
+                    Dth mdl = new Dth();
+                    mdl.DthID = item.DthID;
+                    mdl.Employee = item.Employee;
+                    mdl.TimeCleared = item.TimeCleared;
+                    mdl.TimeEntered = item.TimeEntered;
+                    mdl.PositionID = item.PositionID;
+                    mdl.EmpSent = item.EmpSent;
+                    if (db.Dths != null)
+                    {
+                        db.Dths.Add(mdl);
+                    }
+                }
+
+                var lunchModel = db.Lunches.OrderBy(x => x.LunchTime);
+                foreach (var item in lunchModel)
+                {
+                    Lunch mdl = new Lunch();
+                    mdl.Employee = item.Employee;
+                    mdl.LunchID = item.LunchID;
+                    mdl.PositionID = item.PositionID;
+                    mdl.LunchTime = item.LunchTime;
+                    mdl.LongerLunch = item.LongerLunch;
+                    mdl.Double = item.Double;
+                    mdl.TimeEntered = item.TimeEntered;
+                    mdl.EmpSent = item.EmpSent;
+                    mdl.TimeCleared = item.TimeCleared;
+                    if (db.Lunches != null)
+                    {
+                        db.Lunches.Add(mdl);
+                    }
+                }
+                var positionModel = db.Positions.ToList();
+                foreach (var item in positionModel)
+                {
+                    Position mdl = new Position();
+                    mdl.PositionID = item.PositionID;
+                    mdl.PositionName = item.PositionName;
+                    if (db.Positions != null)
+                    {
+                        db.Positions.Add(mdl);
+                    }
+                }
+
+                ViewBag.EmployeesNames = db.Employees.Where(x => x.NotActive == false).OrderBy(x => x.DisplayName).ToList();
+                ViewBag.PositionNames = db.Positions.OrderBy(x => x.PositionName).ToList();
+                ViewBag.LunchTimes = db.Lunches.Where(x => x.TimeCleared == null).OrderBy(x => x.LunchTime).ToList();
+                ViewBag.Dths = db.Dths.Where(x => x.TimeCleared == null).OrderBy(x => x.TimeEntered).ToList();
+                ViewBag.Breaks = db.Breaks.Where(x => x.TimeCleared == null).OrderBy(x => x.TimeEntered).ToList();
+                ViewBag.Lunches = db.Lunches.Where(x => x.TimeCleared == null).OrderBy(x => x.LunchTime).ToList();
+                ViewBag.breakOffFloor = db.Breaks.Where(x => x.EmpSent == true && x.TimeCleared == null).ToList();
+                ViewBag.dthOffFloor = db.Dths.Where(x => x.EmpSent == true && x.TimeCleared == null).ToList();
+                ViewBag.lunchOffFloor = db.Lunches.Where(x => x.EmpSent == true && x.TimeCleared == null).ToList();
+
+                string partialHtml = Common.RenderRazorViewToString(ControllerContext, "~/Views/Home/_DropDowns.cshtml");
+                return Json(new { success = true, html = partialHtml }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false });
+            }
+        }
+
+        [HttpPost]
         public ActionResult empSentLunch(int lunchID)
         {
             try
